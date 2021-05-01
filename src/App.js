f85f3d9a1c1d32698271d0b1c2da2e9c54b544c2
vmeset <vmeset@gmail.com>
2021-05-01 12:02:38 +0300

    redux on board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router';
 
-function App() {
+import Navbar from './components/Navbar';
+import Buy from './pages/Buy';
+import Done from './pages/Done';
+import Main from './pages/Main';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/done" component={Done} />
+        <Route path="/buy" component={Buy} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
